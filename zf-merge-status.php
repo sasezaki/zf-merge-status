ebe3c870d4756d64ef599e2fd21a885a2a50a292
sasezaki <sasezaki@gmail.com>
2012-10-14 02:43:42 +0900

    zf1 svn repo name is moved to zf.Zend+Framework

--- zf-merge-status.php
+++ zf-merge-status.php
@@ -49,7 +49,7 @@ function check($differ, $onlynotice, $echoCommitMessage){
 
         if ($status->status == ZFMerge_Status::STATUS_NONE) {
 
-            echo "[r$rev](http://framework.zend.com/code/revision.php?repname=Zend+Framework&path=%2Ftrunk&rev=$rev)";  
+            echo "[r$rev](http://framework.zend.com/code/revision.php?repname=zf.Zend+Framework&path=%2Ftrunk&rev=$rev)";  
             echo " ", $status->commit['DATE'], " / ", $status->commit['AUTHOR'];
             if ($onlynotice) {
                 if ($echoCommitMessage) {
@@ -57,7 +57,7 @@ function check($differ, $onlynotice, $echoCommitMessage){
                     echo "<pre>".preg_replace('/^\s+/m', '', rtrim($status->commit["MSG"])), PHP_EOL, "</pre>", PHP_EOL, PHP_EOL;
                     // http://framework.zend.com/code/diff.php?repname=Zend+Framework&path=%2Ftrunk%2Flibrary%2FZend%2FView%2FHelper%2FHeadTitle.php&rev=23387&peg=23387
                     foreach ($status->commit['PATHS'] as $path) {
-                        $href = 'http://framework.zend.com/code/diff.php?repname=Zend+Framework&path='. 
+                        $href = 'http://framework.zend.com/code/diff.php?repname=zf.Zend+Framework&path='. 
                             urlencode(substr($path['PATH'], 9)) . '&rev='. $rev;
                         echo " - {".$path['ACTION']."} [", substr($path['PATH'], 16) ,"]($href)", "\n", PHP_EOL;
                     }
